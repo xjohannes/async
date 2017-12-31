c9c2d3832c09a7b56b9b96b989fdd68b64f37091
Johannes Akse <johannes.akse@gmail.com>
2017-12-31 11:02:27 +0100

    Generators first

--- public/app/main.js
+++ public/app/main.js
@@ -1,4 +1,5 @@
-var promise = function(url) {
+//Promise
+/*var promise = function(url) {
   return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
     xhr.open("GET", url);
@@ -12,13 +13,26 @@ var value = promise('http://localhost:3000/secondAsync');
 
 value
 	.then(function(data) {
-		console.log('SUCCESS ', data )})
+		console.log('SUCCESS ', data )
+	})
 	.catch(function(err) {
 		console.log('ERROR 1', err)
 	})
+*/
+// Error in promises
 /*
 var errValue = promise('http://localhost:3000/errorResponse');
 
 errValue.catch(function(err) {
 	console.log('Error 2', err);
 })*/
+
+// Generators
+function *foo(x) {
+	console.log("INNSIDE pear", yield x);
+	console.log("INNSIDE apple", yield 2);
+}
+
+var gen = foo(3);
+console.log("OUTSIDE:", gen.next("PEAR"));
+console.log("OUTSIDE:", gen.next("APPLE"));
